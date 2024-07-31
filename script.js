a4afc9c37a56b514fefb4a072c90733d8c157cc5
Raj Singh <singhbaghelraj976@gmail.com>
2024-07-31 22:42:03 +0530

    Add files via upload
    Hello friends, my name is Raj Singh. I am from MP city. I am an undergraduate student in which I am doing Bachelor of Computer Application course and I am very interested in web dev in which I learn new technologies.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const typed = new Typed('.moving-el', {
+    strings: ['WebSite Devlopment','Application Devlopment','UI-UX Design','Frontend Devlopment', 'Backend Devlopment', 'java',],
+    typeSpeed: 60,
+    backSpeed:60,
+    backDelay:1000,
+    loop:true,
+  });
